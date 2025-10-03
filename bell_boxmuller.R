cbd2ca38463eedc6987a9cce6ce9e24cddae6dbf
André Savassi <savassiandre@gmail.com>
2025-10-03 15:56:30 -0300

    Adições

--- bell_boxmuller.R
+++ bell_boxmuller.R
@@ -1,3 +1,4 @@
+set.seed(20252)
 # Número de amostras (pares de Normais)
 n <- 10000
 
@@ -9,16 +10,19 @@ z2 <- sqrt(-2 * log(u1)) * sin(2 * pi * u2)
 
 # Junta as duas amostras
 x_bm <- c(z1, z2)
-
+dir.create("figuras", showWarnings = FALSE)
+png("figuras/graf_bell_bm.png", width = 800, height = 600, res = 120)
 # Histograma
 hist(
   x_bm,
   breaks = 50,
   probability = TRUE,
   main = "Normal(0,1) via Box-Muller",
-  col = "lightgreen",
+  col = "darkblue",
   border = "white"
 )
 
 # Curva teórica
 curve(dnorm(x), add = TRUE, col = "red", lwd = 2)
+
+dev.off()
--- bell_inversa.R
+++ bell_inversa.R
@@ -1,3 +1,4 @@
+set.seed(20252)
 # Número de amostras
 n <- 10000
 
@@ -7,15 +8,20 @@ u <- runif(n)
 # Transformação inversa via função quantil
 x_inv <- qnorm(u, mean = 0, sd = 1)
 
+dir.create("figuras", showWarnings = FALSE)
+png("figuras/graf_bell_inv.png", width = 800, height = 600, res = 120)
 # Histograma
 hist(
   x_inv,
   breaks = 50,
   probability = TRUE,
   main = "Normal(0,1) via Transformação Inversa (qnorm)",
-  col = "lightblue",
+  col = "darkblue",
   border = "white"
 )
 
 # Curva teórica
 curve(dnorm(x), add = TRUE, col = "red", lwd = 2)
+
+
+dev.off()
